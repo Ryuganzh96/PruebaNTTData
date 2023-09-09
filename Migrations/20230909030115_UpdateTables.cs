0c8cd3f9a66c51d64bd30b21bb489bdcca77f6bc
Juan Murillo <juan.murillo@accenture.com>
2023-09-08 22:01:39 -0500

    updateMigration

---
+++ Migrations/20230909030115_UpdateTables.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MilitarLogisticsAPI.Migrations
+{
+    public partial class UpdateTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ip",
+                table: "Provisiones_logs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Response",
+                table: "Provisiones_logs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Response",
+                table: "Provisiones_logs",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ip",
+                table: "Provisiones_logs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
